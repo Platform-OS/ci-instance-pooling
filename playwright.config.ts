ab6df257cf29c577cf797a7fe7f566b9a7521d69
damcik <suchuschy@gmail.com>
2025-04-03 12:05:48 +0200

    disable parallel for all tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, devices } from '@playwright/test';
-import process from 'process';
 
 /**
  * Read environment variables from file.
@@ -14,14 +13,8 @@ import process from 'process';
  */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
@@ -32,11 +25,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
     baseURL: process.env.MPKIT_URL,
-
     viewport: null,
-
-    testIdAttribute: 'data-tc',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
   },
